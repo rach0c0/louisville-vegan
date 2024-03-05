1c550fd75c598dd732a3d1e6898c879360900f37
Rachel O <roconnellvisualartist@gmail.com>
2024-03-04 21:47:12 -0500

    Home css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
-
+import './styles/Home.css';
 
 function Home() {
-   return ( <h1>Louisville Vegan</h1>
-   
-   );
-};
+    return(
+        <div className="home-div">
+            <section className="main">
+                <h1>Welcome to foodie'ville</h1>
+                <p className="homeP">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur velit turpis, convallis at malesuada ut, consectetur eget dolor. Maecenas tristique, ante fermentum iaculis commodo, leo lorem facilisis mi, et fermentum lectus metus in nulla. Ut felis massa, vulputate in aliquam in, tempus sit amet tellus. Maecenas a pharetra dui. Donec lacinia quis velit ut placerat. Nulla vitae gravida diam. Nullam at enim placerat, sollicitudin libero non, porta augue. Phasellus viverra, nibh non bibendum volutpat, velit mi convallis quam, at interdum dui dui in ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer sed aliquet sapien. Nullam quis volutpat urna.
+                <br/><br/>
+                Nullam at mollis ante. Integer volutpat urna nisl, sed lobortis massa viverra at. Donec vitae magna odio. Aenean nunc massa, auctor ut scelerisque in, bibendum eu arcu. Praesent rhoncus diam quis eros molestie, id tincidunt ante sagittis. Aliquam mi felis, euismod eget purus a, mattis accumsan neque. Maecenas dictum, nisl et venenatis fermentum, lectus velit hendrerit tortor, quis finibus quam quam vitae sem. Vivamus sollicitudin rutrum libero vitae mollis. Cras et velit a turpis sagittis laoreet ac a magna.
+                <br/><br/>
+                Vestibulum tincidunt ligula non eros feugiat tincidunt. Nulla malesuada lacus nisi, id venenatis metus ultrices in. Cras luctus, enim nec malesuada malesuada, felis odio iaculis dui, tristique finibus lorem eros et ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut massa nec felis interdum lobortis sit amet quis nisl. Phasellus non vulputate lectus. Ut in justo lectus. Donec pulvinar id lectus quis iaculis.
+                </p>
+            </section>
+        </div>
+    )
+}
 
 export default Home;
